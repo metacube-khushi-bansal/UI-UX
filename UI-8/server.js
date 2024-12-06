7a43abef8ab0e2a823970f06bbb1c88e70788b09
Khushi Bansal <khushi.bansal@metacube.com>
2024-12-06 21:32:33 +0530

    updated assignment8

--- UI-8/server.js
+++ UI-8/server.js
@@ -1,11 +1,8 @@
+
 const express = require('express');
 const app = express();
-const PORT = 3000;
-app.get('/', (req,res) =>{
-    res.send('Hello, Node.js Server is running!');
-
-});
-
+app.use(express.static(__dirname + '/sample')); //__dir and not _dir
+var PORT = 3000; // you can use any port
 app.listen(PORT, () =>{
     console.log(`Server is running on http://localhost:${PORT}`);
 })
